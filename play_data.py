a7dd51f13e4c584ec4ebde9ce3d378d32b28b8fb
BitResonant <matteo.caruso.linardon@gmail.com>
2026-04-09 21:57:03 +0200

    Added First Wavetable layer and some of the logic behind input data and parameters mapping

--- play_data.py
+++ play_data.py
@@ -5,7 +5,7 @@ from pythonosc import udp_client
 
 # --- Configuration ---
 CSV_PATH = r"C:\Users\matte\Desktop\audio_ux\EV_UX\CSV_files"
-FILENAME = "17_ENV_Suburban.csv"
+FILENAME = "08_DYN_Acc_100_Percent.csv"
 FULL_PATH = os.path.join(CSV_PATH, FILENAME)
 
 # OSC
